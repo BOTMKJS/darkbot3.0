e19e43d240bc40c72d39f9904ca369454f7221e0
BOTMKJS <80111704+BOTMKJS@users.noreply.github.com>
2021-03-05 11:43:06 -0300

    Update suporte.java

--- Android/suporte.java
+++ Android/suporte.java
@@ -1,9 +1,9 @@
 /*
- * Classname             (darkbot3.0)
+ * Classname             (LJ BOT)
  *
  * Version information   (3.0)
  *
  * Date                  (06/12,15:35)
  *
- * author                (Dark YT)
+ * author                (BRANNKINN)
  * Copyright notice      (bot com suporte a Android/Java)
